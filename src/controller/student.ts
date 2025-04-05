2a951bad37ab52244fc67f460fa5ca27b4b6d987
Javsan0424 <A01198909@tec.mx>
2025-04-04 22:37:00 -0600

    Finished controller

--- src/controller/student.ts
+++ src/controller/student.ts
@@ -16,14 +16,23 @@ class StudentController {
         const student_status_list = [];
 
         for (let student of studentList) {
-            if (student[3] > 90 && !student[4]) {
+            if (student[3] >= 90 && !student[4]) {
                 student_status_list.push([student[1], "Te graduaste con honores"]);
             }
-            else if(student[3] > 90 && student[4]){
+            else if(student[3] >= 90 && student[4]){
                 student_status_list.push([student[1], "Tenias honores pero tienes una deuda"]);
             }
-            else if(student[3] > 70 && student[3] <90 && !student[4]){
+            else if(student[3] > 69 && student[3] <90 && !student[4]){
                 student_status_list.push([student[1], "Pasaste muy apenas"]);
+            }
+            else if(student[3] > 69 && student[3] <90 && student[4]){
+                student_status_list.push([student[1], "Pasaste muy apenas pero tienes deuda"]);
+            }
+            else if(student[3] < 70 && !student[4]){
+                student_status_list.push([student[1], "Como no tienes adeudo tienes derecho a un examen de recuperación."]);
+            }
+            else if(student[3] < 70 && student[4]){
+                student_status_list.push([student[1], "Expulsado."]);
             }
         }
 
